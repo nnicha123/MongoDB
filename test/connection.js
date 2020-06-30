49ea6a1d60a4b68507da4c4ae1ed3c9636e24063
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-06-30 10:17:50 +0700

    representing promise in ES6 and also add before() to finish connection before testing

--- test/connection.js
+++ test/connection.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose')
 
-// connect to mongodb
-mongoose.connect("mongodb://localhost/testaroo");
+// ES6 promises
+mongoose.Promise = global.Promise //using the ES6 default promise instead of mongoose promise (to get rid of warning)
 
-mongoose.connection.once('open',() => {
-    console.log('Connection has been made!')
-}).on('error',(error) => console.log('Connection Error! ', error))
+// Connect to the db before tests run, async so don't know when done, pass done as a parameter and call done after finishes connection
+before((done) => {
+    // connect to mongodb
+    mongoose.connect("mongodb://localhost/testaroo");
+    mongoose.connection.once('open', () => {
+        console.log('Connection has been made!')
+        done()
+    }).on('error', (error) => console.log('Connection Error! ', error)) //on used to watch our for error everytime, once used to establish connection only at the start
+    
+})
