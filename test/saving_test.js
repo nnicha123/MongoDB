86567bf8b39833d76cf518b7bdcb755faf9b41bc
Nicha Ngamtweerat <nicha_nga@hotmail.com>
2020-06-30 10:10:12 +0700

    made a saving_test test file which checks if new collection was created and then saved (async)

---
+++ test/saving_test.js
@@ -0,0 +1,19 @@
+const mocha = require('mocha')
+const assert = require('assert')
+const MarioChar = require('../models/mariochar')
+// Describe tests
+describe('Saving records',() => {
+    // Create tests
+    it('saves a record to the database',(done) => {
+        var char = new MarioChar({
+            name:'Nicha',
+            weight:45
+        })
+        char.save().then(() => {
+            assert(char.isNew === false) //Check that its not new, already saved to database (because in the 'then' - should finish by now)
+            done();
+        })
+    });
+    // Next test
+    
+})
